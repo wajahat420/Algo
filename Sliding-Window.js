89b2afd97498c4cd0bd0e0b7aa2a1c1cc95e4f44
wajahatsarwat <wajahat.sarwat@m3tech.com.pk>
2025-01-07 18:29:45 +0500

    sliding window programs updated with GPT

--- Sliding-Window.js
+++ Sliding-Window.js
@@ -122,8 +122,41 @@ function MaxConsecutiveOnes(nums) {
   console.log({ counter })
 }
 
-// const nums = [1, 0, 1, 1, 0]
-// MaxConsecutiveOnes(nums)
+// approach -2 myself  
+function maxConsecutiveOnes(nums) {
+  let start = 0,
+    length = 0,
+    flip = 0,
+    lastFlipIndex = 0
+
+  for (const [end, rightChar] of nums.entries()) {
+    if (rightChar === 0) {
+      if (flip === 1) lastFlipIndex = end
+
+      flip++
+    }
+
+    if (flip === 2) {
+      start = lastFlipIndex
+      flip--
+    }
+
+
+
+    if (end - start + 1 > length) {
+      length = end - start + 1
+    }
+  }
+}
+
+const nums = [1, 1, 0, 1, 1, 1, 0, 1, 1] // 6 -> second 0
+// const nums = [1, 1, 1, 1, 0, 1, 1, 1, 1] // 9 
+// const nums = [1, 0, 1, 1, 0] // 4 -> first 0
+// const nums = [1, 0, 1, 1, 0, 1] // -> 4 -> any 0
+// const nums = [1, 1, 1, 1, 0] // 5 
+// const nums = [0, 1, 0, 0, 1, 1] // 3 -> third 0
+maxConsecutiveOnes(nums)
+
 
 
 // 6- Longest Subarray with At Most K Distinct Characters
@@ -133,7 +166,7 @@ function LongestSubStringDistinctChars(str, K) {
   for (i in str) {
     i = Number(i)
 
-    let distinct = 1
+    let distinct = 0
     let counter = 1
     while (distinct <= K && counter + i + 1 < str.length) {
       let iter = counter + i
@@ -141,8 +174,8 @@ function LongestSubStringDistinctChars(str, K) {
 
       if (!chars.includes(str[iter - 1])) distinct += 1
 
-      if (chars.length > maxChars) {
-        maxChars = chars.length
+      if (chars.length + 1 > maxChars) {
+        maxChars = chars.length + 1
       }
 
       console.log({ distinct, chars, maxChars, counter, item: str[iter - 1], iter, len: !chars.includes(str[iter - 1]) })
@@ -155,6 +188,142 @@ function LongestSubStringDistinctChars(str, K) {
 
 }
 
-const s = "cbbebi"
-const K = 3
-LongestSubStringDistinctChars(s, K)
+// approach 2 myself + GPT
+function longestSubStringDistinctChars(str, K) {
+  let maxChars = 0,
+    charFreq = {},
+    distinct = 0,
+    start = 0,
+    subStringStart = 0
+
+  for (end in str) {
+    end = Number(end)
+
+    let rightChar = str[end]
+
+    if (!charFreq[rightChar]) distinct += 1
+
+    charFreq[rightChar] = (charFreq[rightChar] || 0) + 1
+
+    while (distinct > K) {
+      let leftChar = str[start]
+      charFreq[leftChar]--
+
+      if (charFreq[leftChar] === 0) distinct--
+      start++
+    }
+  }
+
+  if (end - start + 1 > maxChars) {
+    maxChars = end - start + 1
+    subStringStart = start
+  }
+
+
+  console.log({ maxChars })
+
+}
+
+// const s = "aaaa"
+// const K = 2
+
+// const s = "abcd" // a -> 1
+// const K = 1
+
+// const s = "aabbcc" // aabbcc -> 6
+// const K = 3
+
+// const s = "aabbcc" // aa -> 2
+// const K = 1
+
+// const s = "araaci" // araa -> 4
+// const K = 2
+
+// const s = "cbbebi" // cbbeb -> 5
+// const K = 3
+// longestSubStringDistinctChars(s, K)
+
+
+
+// 7- Find the smallest substring in a given string that contains all the characters of a given pattern.
+
+function SmallestSubStringInAString(str, subStr) {
+  let smalletLen = Infinity + ''
+  for (i in str) {
+    i = Number(i)
+    const subStrLength = subStr.length
+
+    let counter = 0
+    while (subStrLength + i + counter <= str.length) {
+      let slicedStr = str.slice(i, subStrLength + i + counter)
+
+      let isCorrectSlicedStr = true
+      for (s of subStr) {
+        if (!slicedStr.includes(s)) isCorrectSlicedStr = false
+      }
+
+      if (isCorrectSlicedStr && slicedStr.length < smalletLen.length) {
+        smalletLen = slicedStr
+        break
+      }
+
+      counter++
+    }
+  }
+
+  console.log({ smalletLen })
+}
+
+// approach 2 -GPT
+function smallestSubstringContainingPattern(str, pattern) {
+  let charFrequency = {};
+  for (let char of pattern) {
+    charFrequency[char] = (charFrequency[char] || 0) + 1;
+  }
+
+  let start = 0,
+    matched = 0,
+    minLength = Infinity,
+    substringStart = 0;
+
+  for (let end = 0; end < str.length; end++) {
+    let rightChar = str[end];
+    if (rightChar in charFrequency) {
+      charFrequency[rightChar]--;
+      if (charFrequency[rightChar] >= 0) {
+        matched++;
+      }
+    }
+
+    console.log({ rightChar, start, end, charFrequency, matched })
+
+
+    while (matched === pattern.length) {
+      if (end - start + 1 < minLength) {
+        minLength = end - start + 1;
+        substringStart = start;
+      }
+
+      let leftChar = str[start];
+
+      start++;
+      if (leftChar in charFrequency) {
+        if (charFrequency[leftChar] === 0) {
+          matched--;
+        }
+        charFrequency[leftChar]++;
+      }
+
+      console.log({ leftChar, matched })
+
+    }
+  }
+
+  return minLength === Infinity ? "" : str.substring(substringStart, substringStart + minLength);
+}
+
+
+// const str = 'abdcabc' // cab
+// const subStr = 'abc'
+
+// console.log(smallestSubstringContainingPattern(str, subStr))
